c8a7124f2a262d88836e1c7a8d3901996876d7b8
Nenad Pejcic <nenad.pejcicc@gmail.com>
2019-05-01 11:51:57 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -10,14 +10,44 @@ class App extends Component {
       countHeads: 0,
       countTails: 0
     }
+
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  headOrTails() {
+    let random = Math.floor(Math.random() * 2)
+    return random === 1 ? true : false; 
+  }
+
+  handleClick() {
+    let res = this.headOrTails()
+
+    if (res) {
+      this.setState((prevState) => {
+        return {
+          heads: res,
+          countFlips: prevState.countFlips + 1,
+          countHeads: prevState.countHeads + 1,
+        }
+      })
+    } else {
+      this.setState((prevState) => {
+        return {
+          heads: res,
+          countFlips: prevState.countFlips + 1,
+          countTails: prevState.countTails + 1,
+        }
+      })
+    }
+  }
+  
   render() { 
     return ( 
       <>
-      <h1>Let's flip a coin</h1>
-      <Coin side={this.state.heads} />
-      <button>Flip me</button>
-      <p>Out of {this.state.countFlips}, there have been {this.state.countHeads} heads and {this.state.countTails} tails.</p>
+        <h1>Let's flip a coin</h1>
+        <Coin side={this.state.heads} />
+        <button onClick={this.handleClick}>Flip me</button>
+        <h3>Out of {this.state.countFlips}, there have been {this.state.countHeads} heads and {this.state.countTails} tails.</h3>
       </>
      );
   }
