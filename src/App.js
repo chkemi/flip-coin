30ed5d31278d58ff54fb3070fedac454a780ffc8
Nenad Pejcic <nenad.pejcicc@gmail.com>
2019-05-01 12:06:09 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       heads: true,
       countFlips: 0,
       countHeads: 0,
-      countTails: 0
+      countTails: 0,
+      flipping: false,
+      disabled: false,
     }
 
     this.handleClick = this.handleClick.bind(this);
@@ -21,32 +23,43 @@ class App extends Component {
 
   handleClick() {
     let res = this.headOrTails()
+    
+    this.setState({
+      flipping: true,
+      disabled: true,
+    })
 
-    if (res) {
-      this.setState((prevState) => {
-        return {
-          heads: res,
-          countFlips: prevState.countFlips + 1,
-          countHeads: prevState.countHeads + 1,
-        }
-      })
-    } else {
-      this.setState((prevState) => {
-        return {
-          heads: res,
-          countFlips: prevState.countFlips + 1,
-          countTails: prevState.countTails + 1,
-        }
-      })
-    }
+    setTimeout(() => {
+      if (res) {
+        this.setState((prevState) => {
+          return {
+            heads: res,
+            countFlips: prevState.countFlips + 1,
+            countHeads: prevState.countHeads + 1,
+            flipping: false,
+            disabled: false,
+          }
+        })
+      } else {
+        this.setState((prevState) => {
+          return {
+            heads: res,
+            countFlips: prevState.countFlips + 1,
+            countTails: prevState.countTails + 1,
+            flipping: false,
+            disabled: false,
+          }
+        })
+      }
+    }, 800)
   }
   
   render() { 
     return ( 
       <>
         <h1>Let's flip a coin</h1>
-        <Coin side={this.state.heads} />
-        <button onClick={this.handleClick}>Flip me</button>
+        <Coin side={this.state.heads} flipping={this.state.flipping} />
+        <button onClick={this.handleClick} disabled={this.state.disabled}>{this.state.disabled ? 'Flipping...' : 'Flip me'}</button>
         <h3>Out of {this.state.countFlips}, there have been {this.state.countHeads} heads and {this.state.countTails} tails.</h3>
       </>
      );
--- src/Coin.js
+++ src/Coin.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Coin = (props) => {
     return ( 
         props.side
-        ? <img src='https://upload.wikimedia.org/wikipedia/commons/c/cd/S_Half_Dollar_Obverse_2016.jpg' alt='Heads' />
-        : <img src='http://www.pcgscoinfacts.com/UserImages/71009269r.jpg' alt='Tails' />
+        ? <img className={props.flipping ? 'flipping' : ''} src='https://upload.wikimedia.org/wikipedia/commons/c/cd/S_Half_Dollar_Obverse_2016.jpg' alt='Heads' />
+        : <img className={props.flipping ? 'flipping' : ''} src='http://www.pcgscoinfacts.com/UserImages/71009269r.jpg' alt='Tails' />
      );
 }
  
